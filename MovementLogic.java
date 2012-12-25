a8e1e4c1f8c06cd628d7c241103880ce3eb2f034
Matti Nikki <muzzy@iki.fi>
2012-12-25 12:52:42 +0200

    add tiebreaker, count road bonus when entering tile instead of when leaving

--- MovementLogic.java
+++ MovementLogic.java
@@ -3,14 +3,17 @@ import cern.colt.list.IntArrayList;
 class MovementLogic {
 	MovementMap map;
 	int width, height, depth;
-	
+
 	public MovementLogic(MovementMap map) {
 		this.map = map;
 		width = map.width();
 		height = map.height();
 		depth = map.depth();
 	}
-	
+
+	// Expected maximum path length used as multiplier for tiebreaking purposes
+	private final static int maxPath = 10000;
+
 	public int heuristic(int idx1, int idx2) {
 		int x1 = idx1%width;
 		int y1 = idx1%(width*height)/width;
@@ -18,29 +21,36 @@ class MovementLogic {
 		int x2 = idx2%width;
 		int y2 = idx2%(width*height)/width;
 		int z2 = idx2/(width*height);
-		// assume roads available for the whole path
-		return 5 * Math.max(Math.max(Math.abs(x2-x1), Math.abs(y2-y1)), Math.abs(z2-z1));
+		int dx = Math.abs(x2-x1);
+		int dy = Math.abs(y2-y1);
+		int dz = Math.abs(z2-z1);
+		// diagonal distance
+		int h1 = Math.max(Math.max(dx, dy), dz);
+		int h = (5 * h1);
+		// add tiebreaker
+		return h * maxPath + h1;
 	}
-	
+
 	public int distance(int idx1, int idx2) {
-		if ((map.getFlags(idx1) & MovementMap.ROAD) != 0) {
-			return 5;
+		// prefer stepping into road tiles
+		if ((map.getFlags(idx2) & MovementMap.ROAD) != 0) {
+			return 5*maxPath;
 		} else {
-			return 10;
+			return 10*maxPath;
 		}
 	}
-	
+
 	protected void consider(IntArrayList list, int idx) {
 		if ((map.getFlags(idx) & MovementMap.WALKABLE) != 0) {
 			list.add(idx);
 		}
 	}
-	
+
 	public int[] getNeighbors(int idx) {
 		int x = idx%width;
 		int y = idx%(width*height)/width;
 		int z = idx/(width*height);
-		
+
 		IntArrayList neighbors = new IntArrayList();
 		if (x>0) consider(neighbors, idx-1);
 		if (y>0) consider(neighbors, idx-width);
@@ -51,7 +61,7 @@ class MovementLogic {
 		if (x<width-1 && y<height-1) consider(neighbors, idx+1+width);
 		if (y<height-1 && x>0) consider(neighbors, idx+width-1);
 		// TODO z-axis ... what's the s() function in obfuscated code?
-		
+
 		neighbors.trimToSize();
 		return neighbors.elements();
 	}
